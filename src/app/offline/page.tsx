6a0d55927c4bba5d8e3a03b89046a8680b584f54
Nuckle <coil@itscat.org>
2024-12-17 14:54:54 +0300

    Update pwa

---
+++ src/app/offline/page.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { Button } from "@nextui-org/button";
+import Head from "next/head";
+
+export default function Offline() {
+    return (
+        <>
+            <Head>
+                <title>Offline</title>
+            </Head>
+            <div className="p-6 flex flex-col items-center justify-center text-center">
+                <h1 className="text-3xl mb-4">You are offline</h1>
+                <p className="mb-6">
+                    It looks like you're not connected to the internet. Please check your network and try again.
+                </p>
+                <Button 
+                    onPress={() => location.reload()} 
+                >
+                    Retry Connection
+                </Button>
+            </div>
+        </>
+    );
+}
