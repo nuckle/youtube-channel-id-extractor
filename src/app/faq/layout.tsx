74c4a77a1c828e8cff529e7e4b2e791b8be495d7
Nuckle <coil@itscat.org>
2024-12-17 11:54:16 +0300

    Add additional meta information

---
+++ src/app/faq/layout.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'FAQ',
+    openGraph: {
+        title: 'FAQ'
+    },
+}
+
+export default function FaqLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return children
+}
