1a61f701048ad3407701b8f0d9ea3f7242302da5
Nuckle <coil@itscat.org>
2024-12-17 15:43:32 +0300

    Add Skeleton component

--- src/app/components/ChannelInfo.tsx
+++ src/app/components/ChannelInfo.tsx
@@ -2,12 +2,15 @@ import { Button } from "@nextui-org/button";
 import { Card, CardBody, CardHeader } from "@nextui-org/card";
 import { Divider } from "@nextui-org/divider";
 import { Link } from "@nextui-org/link";
+import { Skeleton } from "@nextui-org/skeleton";
 import { Check, Copy } from 'lucide-react';
 import { useState } from 'react';
+import { useFormStatus } from "react-dom";
 import { ChannelDataType } from "../types/channelType";
 
 export default function ChannelInfo({ id, channelUrl, rssUrl }: ChannelDataType) {
 
+    const { pending } = useFormStatus();
     const [isCopied, setIsCopied] = useState(false)
 
     const copyToClipboard = async (text: string) => {
@@ -29,44 +32,50 @@ export default function ChannelInfo({ id, channelUrl, rssUrl }: ChannelDataType)
             <CardBody>
                 <div className='mb-2'>
                     <p className="text-sm text-muted-foreground">Channel ID:</p>
-                    <div className="flex items-center space-x-2">
-                        <p className='text-sm font-medium'>{id}</p>
-                        <Button
-                            size='sm'
-                            isIconOnly
-                            onPress={() => copyToClipboard('UCa9eML87RIvw8vRD92n81UA')}
-                            aria-label="Copy to clipboard"
-                            className="min-w-[24px] w-[24px] h-[24px] p-0"
-                        >
-                            {isCopied ? (
-                                <Check className="h-3 w-3" />
-                            ) : (
-                                <Copy className="h-3 w-3" />
-                            )}
-                        </Button>
-                    </div>
+                    <Skeleton className="flex items-center space-x-2" isLoaded={!pending}>
+                        <div className="flex items-center space-x-2">
+                            <p className='text-sm font-medium'>{id}</p>
+                            <Button
+                                size='sm'
+                                isIconOnly
+                                onPress={() => copyToClipboard('UCa9eML87RIvw8vRD92n81UA')}
+                                aria-label="Copy to clipboard"
+                                className="min-w-[24px] w-[24px] h-[24px] p-0"
+                            >
+                                {isCopied ? (
+                                    <Check className="h-3 w-3" />
+                                ) : (
+                                    <Copy className="h-3 w-3" />
+                                )}
+                            </Button>
+                        </div>
+                    </Skeleton>
                 </div>
                 <div className='mb-2'>
                     <p className="text-sm text-muted-foreground mb-1">Channel URL:</p>
-                    <Link
-                        isExternal
-                        showAnchorIcon
-                        href={channelUrl}
-                        className="whitespace-nowrap"
-                    >
-                        {channelUrl}
-                    </Link>
+                    <Skeleton isLoaded={!pending}>
+                        <Link
+                            isExternal
+                            showAnchorIcon
+                            href={channelUrl}
+                            className="whitespace-nowrap"
+                        >
+                            {channelUrl}
+                        </Link>
+                    </Skeleton>
                 </div>
                 <div>
                     <p className="text-sm text-muted-foreground mb-1">RSS Feed URL:</p>
-                    <Link
-                        isExternal
-                        showAnchorIcon
-                        href={rssUrl}
-                        className="whitespace-nowrap"
-                    >
-                        {rssUrl}
-                    </Link>
+                    <Skeleton isLoaded={!pending}>
+                        <Link
+                            isExternal
+                            showAnchorIcon
+                            href={rssUrl}
+                            className="whitespace-nowrap"
+                        >
+                            {rssUrl}
+                        </Link>
+                    </Skeleton>
                 </div>
             </CardBody>
         </Card>
