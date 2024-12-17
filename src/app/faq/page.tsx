b18c7fa35a77213f4fd650f560324340894ddded
Nuckle <coil@itscat.org>
2024-12-17 11:15:52 +0300

    Update FAQ page

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -14,6 +14,19 @@ export default function Faq() {
                 <h3 className="font-semibold">How do I use this tool?</h3>
                 <p>Simply paste the URL of a YouTube channel into the input field on the home page and click the search button. The tool will extract and display the channel ID for you.</p>
             </section>
+
+            <section className="mb-6">
+                <h3 className="font-semibold">Supported URL formats</h3>
+                <p>This tool supports the following URL formats</p>
+                <ul>
+                    <li>https://www.youtube.com/channel/channel_id</li>
+                    <li>https://www.youtube.com/user/username</li>
+                    <li>https://www.youtube.com/@channel_handle</li>
+                    <li>https://www.youtube.com/c/custom_url</li>
+                </ul>
+                <p>Also the tool supports links from Wayback Machine</p>
+                <li>https://web.archive.org/web/20221218220114/https://www.youtube.com/@channel_handle</li>
+            </section>
         </>
     )
 }
